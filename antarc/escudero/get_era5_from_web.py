5a7e7761487c8cf13e65035cbd98643fbd0c57f3
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-02-02 13:04:55 -0800

    Functinalize all general code

--- antarc/escudero/get_era5_from_web.py
+++ antarc/escudero/get_era5_from_web.py
@@ -13,42 +13,132 @@ from antarc.escudero.parameters import esc_params
 from antarc.escudero.parameters import esc202202
 
 
-def get_era5():
+def get_params(datestr: list[str], times, latlon, press_levs):
+    """
+    Get the ERA5 data as indicated in params from the web
+    @returns  A list of the parameter structures
+    @returns  The output file names
+    """
+    # api parameters:
+    # area: [north, west, south, east]; west is negative
+    return {
+        "format": "netcdf",
+        "product_type": "reanalysis",
+        "variable": [
+            "pressure",
+            "Geopotential",
+            "Temperature",
+            "Relative humidity",
+            "Ozone mass mixing ratio",
+        ],
+        "pressure_level": press_levs,
+        "year": [datestr[0]],
+        "month": [datestr[1]],
+        "day": [datestr[2]],
+        "time": times,
+        "grid": [0.25, 0.25],
+        "area": [
+            latlon["north"],
+            latlon["west"],
+            latlon["south"],
+            latlon["east"],
+        ],
+    }
+
+
+def get_outfile(prefix: str, datestring: str, ext: str = ".nc"):
+    """
+    Get the name of the output file
+    @param  prefix  Desired beginging of file name
+    @param  datestring  Date string
+    @param  ext  Extension, beginning with '.'; e.g. '.nc'
+    """
+    return prefix + datestring + ext
+
+
+def get_paramsets(
+    prefix,
+    yearstr,
+    monthstr,
+    days,
+    times,
+    latlon,
+    press_levs,
+):
+    """
+    Get dictionaries with the output file names and the parameters
+    dictionary of parameters needed for retrieving ERA5 data
+    and pack it together into
+    @returns  A list of the parameter structures
+    @returns  The output file names
+    """
+
+    paramsets = []
+    # TODO: remove [:1] below
+    for day in days[:1]:
+        daystr = str(day).zfill(2)
+        datestring = daystr
+
+        paramset = get_paramset(
+            prefix,
+            yearstr,
+            monthstr,
+            day,
+            times,
+            latlon,
+            press_levs,
+        )
+
+        paramsets.append(paramset)
+    return paramsets
+
+
+def get_paramset(
+    prefix,
+    yearstr,
+    monthstr,
+    day,
+    times,
+    latlon,
+    press_levs,
+):
+    """
+    Get dictionaries with the output file names and the parameters
+    dictionary of parameters needed for retrieving ERA5 data
+    and pack it together into
+    @returns  A list of the parameter structures
+    @returns  The output file names
+    """
+
+    daystr = str(day).zfill(2)
+    datestring = daystr
+    outfile = get_outfile(prefix, datestring, ".nc")
+
+    # api parameters:
+    # area: [north, west, south, east]; west is negative
+    params = get_params(
+        [yearstr, monthstr, daystr],
+        times,
+        latlon,
+        press_levs,
+    )
+
+    return {"outfile": outfile, "params": params}
+
+
+def get_era5(outdir, paramsets, dataset):
     """
     Get the ERA5 data as indicated in params from the web
     """
-    c = cdsapi.Client()
-    for day in days[:1]:
-        daystr = str(day).zfill(2)
-        outfile = "era5_esc_202202" + daystr + ".nc"
-
-        # api parameters:
-        # area: [north, west, south, east]; west is negative
-        params = {
-            "format": "netcdf",
-            "product_type": "reanalysis",
-            "variable": [
-                "pressure",
-                "Geopotential",
-                "Temperature",
-                "Relative humidity",
-                "Ozone mass mixing ratio",
-            ],
-            "pressure_level": press_levs,
-            "year": [yearstr],
-            "month": [monthstr],
-            "day": [daystr],
-            "time": times,
-            "grid": [0.25, 0.25],
-            "area": [north, west, south, east],
-        }
+    c_era = cdsapi.Client()
+    for paramset in paramsets:
 
         # retrieve the path to the file
-        fl = c.retrieve(dataset, params)
+        fid = c_era.retrieve(dataset, paramset["params"])
 
-        # download the file
+        # download the file and save it to the output directory
         if download_flag:
-            fl.download(outdir + outfile)
+            fid.download(outdir + paramset["outfile"])
 
 
 LATITUDE = esc_params.LATITUDE
@@ -62,9 +152,13 @@ DATE2 = esc202202.DATE2
 outdir = "/Users/prowe/Sync/measurements/Escudero/era5/"
 
 north = np.round(LATITUDE / 0.25) * 0.25 + 0.25
-south = north - 0.25
 east = np.round(LONGITUDE / 0.25) * 0.25 + 0.25
-west = east - 0.5
+latlon = {
+    "north": north,
+    "south": north - 0.25,
+    "east": east,
+    "west": east - 0.5,
+}
 
 # Pressure levels
 press_levs = np.hstack(
@@ -86,4 +180,15 @@ yearstr = str(DATE1.year)
 monthstr = str(DATE1.month).zfill(2)
 days = list(range(DATE1.day - 1, DATE2.day + 2))  # pad out a day on each side
 
-get_era5()
+prefix = "era5_esc_202202"
+
+paramsets = get_paramsets(
+    prefix,
+    yearstr,
+    monthstr,
+    days,
+    times,
+    latlon,
+    press_levs,
+)
+get_era5(outdir, paramsets, dataset)
