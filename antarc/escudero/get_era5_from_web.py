e7b029b31834fe843f38fc27f8aeda1a032be915
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-02-02 13:28:56 -0800

    Move functions to seperate code

--- antarc/escudero/get_era5_from_web.py
+++ antarc/escudero/get_era5_from_web.py
@@ -76,8 +76,6 @@ def get_paramsets(
     paramsets = []
     # TODO: remove [:1] below
     for day in days[:1]:
-        daystr = str(day).zfill(2)
-        datestring = daystr
 
         paramset = get_paramset(
             prefix,
@@ -130,6 +128,7 @@ def get_era5(outdir, paramsets, dataset):
     """
     Get the ERA5 data as indicated in params from the web
     """
+    download_flag = True
     c_era = cdsapi.Client()
     for paramset in paramsets:
 
@@ -141,54 +140,54 @@ def get_era5(outdir, paramsets, dataset):
             fid.download(outdir + paramset["outfile"])
 
 
-LATITUDE = esc_params.LATITUDE
-LONGITUDE = esc_params.LONGITUDE
-ALTITUDE = esc_params.ALTITUDE
+# LATITUDE = esc_params.LATITUDE
+# LONGITUDE = esc_params.LONGITUDE
+# ALTITUDE = esc_params.ALTITUDE
 
-DATE1 = esc202202.DATE1
-DATE2 = esc202202.DATE2
+# DATE1 = esc202202.DATE1
+# DATE2 = esc202202.DATE2
 
 
-outdir = "/Users/prowe/Sync/measurements/Escudero/era5/"
+# outdir = "/Users/prowe/Sync/measurements/Escudero/era5/"
 
-north = np.round(LATITUDE / 0.25) * 0.25 + 0.25
-east = np.round(LONGITUDE / 0.25) * 0.25 + 0.25
-latlon = {
-    "north": north,
-    "south": north - 0.25,
-    "east": east,
-    "west": east - 0.5,
-}
+# north = np.round(LATITUDE / 0.25) * 0.25 + 0.25
+# east = np.round(LONGITUDE / 0.25) * 0.25 + 0.25
+# latlon = {
+#     "north": north,
+#     "south": north - 0.25,
+#     "east": east,
+#     "west": east - 0.5,
+# }
 
-# Pressure levels
-press_levs = np.hstack(
-    [
-        np.arange(1000, 925 - 5, -25),
-        np.arange(900, 200, -50),
-        np.arange(200, 100 - 5, -25),
-    ]
-)
-press_levs_ints = list(press_levs) + [70, 50, 30, 20, 10, 7, 5, 3, 2]
-press_levs = [str(p) for p in press_levs_ints]
+# # Pressure levels
+# press_levs = np.hstack(
+#     [
+#         np.arange(1000, 925 - 5, -25),
+#         np.arange(900, 200, -50),
+#         np.arange(200, 100 - 5, -25),
+#     ]
+# )
+# press_levs_ints = list(press_levs) + [70, 50, 30, 20, 10, 7, 5, 3, 2]
+# press_levs = [str(p) for p in press_levs_ints]
 
-# Times
-times = [f"0{i}:00" for i in range(10)] + [f"{i}:00" for i in range(10, 24)]
-dataset = "reanalysis-era5-pressure-levels"
-download_flag = True
+# # Times
+# times = [f"0{i}:00" for i in range(10)] + [f"{i}:00" for i in range(10, 24)]
+# dataset = "reanalysis-era5-pressure-levels"
+# download_flag = True
 
-yearstr = str(DATE1.year)
-monthstr = str(DATE1.month).zfill(2)
-days = list(range(DATE1.day - 1, DATE2.day + 2))  # pad out a day on each side
+# yearstr = str(DATE1.year)
+# monthstr = str(DATE1.month).zfill(2)
+# days = list(range(DATE1.day - 1, DATE2.day + 2))  # pad out a day on each side
 
-prefix = "era5_esc_202202"
+# prefix = "era5_esc_202202"
 
-paramsets = get_paramsets(
-    prefix,
-    yearstr,
-    monthstr,
-    days,
-    times,
-    latlon,
-    press_levs,
-)
-get_era5(outdir, paramsets, dataset)
+# paramsets = get_paramsets(
+#     prefix,
+#     yearstr,
+#     monthstr,
+#     days,
+#     times,
+#     latlon,
+#     press_levs,
+# )
+# get_era5(outdir, paramsets, dataset)
---
+++ antarc/escudero/get_era5_from_web_esc.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 11:14:02 2023
+
+@author: prowe
+"""
+
+import numpy as np
+
+from antarc.escudero.parameters import esc_params
+from antarc.escudero.parameters import esc202202
+from antarc.escudero.get_era5_from_web import get_paramsets, get_era5
+
+
+LATITUDE = esc_params.LATITUDE
+LONGITUDE = esc_params.LONGITUDE
+ALTITUDE = esc_params.ALTITUDE
+
+DATE1 = esc202202.DATE1
+DATE2 = esc202202.DATE2
+
+
+outdir = "/Users/prowe/Sync/measurements/Escudero/era5/"
+
+north = np.round(LATITUDE / 0.25) * 0.25 + 0.25
+east = np.round(LONGITUDE / 0.25) * 0.25 + 0.25
+latlon = {
+    "north": north,
+    "south": north - 0.25,
+    "east": east,
+    "west": east - 0.5,
+}
+
+# Pressure levels
+press_levs = np.hstack(
+    [
+        np.arange(1000, 925 - 5, -25),
+        np.arange(900, 200, -50),
+        np.arange(200, 100 - 5, -25),
+    ]
+)
+press_levs_ints = list(press_levs) + [70, 50, 30, 20, 10, 7, 5, 3, 2]
+press_levs = [str(p) for p in press_levs_ints]
+
+# Times
+times = [f"0{i}:00" for i in range(10)] + [f"{i}:00" for i in range(10, 24)]
+dataset = "reanalysis-era5-pressure-levels"
+download_flag = True
+
+yearstr = str(DATE1.year)
+monthstr = str(DATE1.month).zfill(2)
+days = list(range(DATE1.day - 1, DATE2.day + 2))  # pad out a day on each side
+
+prefix = "era5_esc_202202"
+
+paramsets = get_paramsets(
+    prefix,
+    yearstr,
+    monthstr,
+    days,
+    times,
+    latlon,
+    press_levs,
+)
+get_era5(outdir, paramsets, dataset)
