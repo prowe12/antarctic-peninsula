461ea9cba9c911c70c67f096848821319d9d2268
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-02-02 10:02:20 -0800

    functionalize code to make plots

--- antarc/escudero/case202202/make_plots_broadband.py
+++ antarc/escudero/case202202/make_plots_broadband.py
@@ -25,6 +25,178 @@ from antarc.escudero.parameters import swd_params
 
 # from antarc.escudero.case202202.get_swd_flux import get_swd
 
+
+def plot_measured_and_clear():
+    rot = 0
+    datefmt = "%d"
+    plt.figure(num=1)
+    ax1 = plt.subplot(211)
+    ax3 = plt.subplot(212, sharex=ax1)
+    wid = 0.85
+    hit = 0.4
+    ax1.set_position([0.12, 0.57, wid, hit])
+    ax3.set_position([0.12, 0.10, wid, hit])
+
+    ax1.plot(swd_date_long[:iswl], swd_long[:iswl], "r", label="Measured")
+    ax1.plot(swd_date_long[iswl:], swd_long[iswl:], color="red")
+    ax1.plot(
+        swd_date_long[:iswl], swd_clear_lib_long[:iswl], "k--", label="Clear"
+    )
+    ax1.plot(swd_date_long[iswl:], swd_clear_lib_long[iswl:], "k--")
+    # global down
+    # ax1.plot(swd_date[ind], swd_clear_lib[:, 0], "ks")  # direct
+    # ax1.plot(swd_date[ind], swd_clear_lib[:, 2], "c.")  # diffuse down
+    # ax1.plot(swd_date[ind], swd_clear_lib[:, 3], "m+")  # diffuse up
+    ax1.legend()
+    ax1.set_xlim(
+        dt.datetime(2022, 2, 5, tzinfo=utc),
+        dt.datetime(2022, 2, 11, tzinfo=utc),
+    )
+    ax1.set_ylim([0, 1000])
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter(datefmt))
+    ax1.tick_params(axis="x", rotation=rot)
+    ax1.set_ylabel("SWD (W/m$^{2}$)")
+    # ax1.legend(loc=(0.1,.3))
+
+    ax3.plot(lwd_date[:ilw], lwd[:ilw], color="blue", label="Measured")
+    ax3.plot(lwd_date[ilw:], lwd[ilw:], color="blue")
+    ax3.plot(lwd_clear_date, lwd_clear, "o:", color="blue", label="Clear")
+    ax3.legend()
+    ax3.xaxis.set_major_formatter(mdates.DateFormatter(datefmt))
+    ax3.tick_params(axis="x", rotation=rot)
+    ax3.set_xlabel("Day of 2022/02")
+    ax3.set_ylabel("LWD (W/m$^{2}$)")
+    ax3.set_ylim([0, 1000])
+    ax3.legend(loc="upper left")
+
+    if savefigs:
+        plt.savefig(fig1name)
+
+
+def plot_meas_clear_forcings_cumulative_shorttime():
+    rot = 40
+    # datefmt = "%d %H"
+    # fdate = swd_date[isw - 1]
+    # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    plt.close(2)
+    plt.figure(num=2)
+    ax1 = plt.subplot(221)
+    ax2 = plt.subplot(222, sharex=ax1)
+    ax3 = plt.subplot(223, sharex=ax1)
+    ax4 = plt.subplot(224, sharex=ax1)
+    wid = 0.37
+    hit = 0.36
+    ax1.set_position([0.1, 0.62, wid, hit])
+    ax2.set_position([0.6, 0.62, wid, hit])
+    ax3.set_position([0.1, 0.15, wid, hit])
+    ax4.set_position([0.6, 0.15, wid, hit])
+
+    ax1.plot(swd_date[:isw], swd[:isw], color="red", label="Meas")
+    ax1.plot(swd_date[:isw], swd_clear_lib_interp[:isw], "k--", label="Clear")
+    ax1.plot(swd_date[isw:], swd_clear_lib_interp[isw:], "k--")
+    ax1.legend()
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter("%d %H"))
+    ax1.tick_params(axis="x", rotation=rot)
+    ax1.set_xlim(start_date, final_date)
+    ax1.set_ylabel("SWD (W/m$^{2}$)")
+    ax1.legend(loc=[0.4, 0.7])
+
+    ax3.plot(lwd_date[:ilw], lwd[:ilw], color="blue", label="Measured")
+    ax3.plot(lwd_clear_date, lwd_clear, "o:", color="blue", label="Clear")
+    ax3.legend()
+    ax3.xaxis.set_major_formatter(mdates.DateFormatter("%d %H"))
+    ax3.tick_params(axis="x", rotation=rot)
+    ax3.set_xlabel("Day and Hour in 2022")
+    ax3.set_ylabel("LWD (W/m$^{2}$)")
+
+    tot_force = swd_force[:isw] + lwd_force_sw_date[:isw]
+    ax2.plot(lwd_date[:ilw], lwd_force[:ilw], color="blue", label="LWD")
+    ax2.plot(swd_date[:isw], swd_force[:isw], color="red", label="SWD")
+    ax2.plot(swd_date[:isw], tot_force, "k", label="Total")
+    ax2.plot(lwd_date[:ilw], np.zeros(ilw), "k:")
+    ax2.legend()
+    ax2.xaxis.set_major_formatter(mdates.DateFormatter("%m/%d %H"))
+    ax2.xaxis.set_major_formatter(mdates.DateFormatter("%d %H"))
+    ax2.tick_params(axis="x", rotation=rot)
+    ax2.set_ylabel("Forcing (W/m$^{2}$)")
+    ax2.legend(loc=[0.4, 0.02])
+
+    ax4.plot(lwd_date[il1:il2], lwf_tot / 1e6, color="blue", label="LWD")
+    ax4.plot(swd_date[is1:is2], swf_tot / 1e6, color="red", label="SWD")
+    ax4.plot(swd_date[is1:is2], totf / 1e6, "k", label="Total")
+    ax4.plot(swd_date[is2], totf[-1] / 1e6, "ko")
+    ax4.plot(lwd_date[il1:il2], np.zeros(il2 - il1), "k:")
+    ax4.legend()
+    ax4.set_ylabel("Cumulative Forcing (MJ/m$^{2}$)")
+    ax4.xaxis.set_major_formatter(mdates.DateFormatter("%d %H"))
+    ax4.tick_params(axis="x", rotation=rot)
+    ax4.set_xlabel("Day and Hour in 2022")
+    ax4.text(
+        dt.datetime(2022, 2, 7, 15, 30, tzinfo=utc), -15, totfstr, fontsize=14
+    )
+
+    if savefigs:
+        plt.savefig(fig2name)
+
+    # Add pysolar result for comparison
+    # ax1.plot(
+    #     swd_date[:isw], swd_pysolar[:isw], "--", color="blue", label="Pysolar"
+    # )
+    # ax1.legend()
+
+
+def plot_meas_clear_forcings():
+    rot = 40
+    # datefmt = "%d %H"
+    # fdate = swd_date[isw - 1]
+    # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    plt.figure(num=3, figsize=[6.5, 5.6])
+    plt.clf()
+    ax1 = plt.subplot(311)
+    ax2 = plt.subplot(312, sharex=ax1)
+    ax3 = plt.subplot(313, sharex=ax1)
+    wid = 0.84
+    ax1.set_position([0.13, 0.67, wid, 0.30])
+    ax2.set_position([0.13, 0.45, wid, 0.18])
+    ax3.set_position([0.13, 0.09, wid, 0.33])
+
+    ax1.plot(swd_date[:isw], swd[:isw], color="red", label="Meas")
+    ax1.plot(swd_date[:isw], swd_clear_lib_interp[:isw], "k--", label="Clear")
+    ax1.plot(swd_date[isw:], swd_clear_lib_interp[isw:], "k--")
+    ax1.legend()
+    ax1.set_ylim([-2, 1000])
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter("%d %H"))
+    ax1.tick_params(axis="x", rotation=rot, labelcolor="white")
+    ax1.set_xlim(start_date, final_date)
+    ax1.set_ylabel("SWD (W/m$^{2}$)")
+    ax1.legend()  # loc=[0.4, 0.7])
+
+    ax2.plot(lwd_date[:ilw], lwd[:ilw], color="blue", label="Measured")
+    ax2.plot(lwd_clear_date, lwd_clear, "o:", color="blue", label="Clear")
+    ax2.set_ylim([0, 400])
+    ax2.legend()
+    ax2.xaxis.set_major_formatter(mdates.DateFormatter("%d %H"))
+    ax2.tick_params(axis="x", rotation=rot, labelcolor="white")
+    ax2.set_xlabel("Day and Hour in 2022")
+    ax2.set_ylabel("LWD (W/m$^{2}$)")
+
+    tot_force = swd_force[:isw] + lwd_force_sw_date[:isw]
+    ax3.plot(lwd_date[:ilw], lwd_force[:ilw], color="blue", label="LWD")
+    ax3.plot(swd_date[:isw], swd_force[:isw], color="red", label="SWD")
+    ax3.plot(swd_date[:isw], tot_force, "k", label="Total")
+    ax3.plot(lwd_date[:ilw], np.zeros(ilw), "k:")
+    ax3.legend()
+    # ax3.xaxis.set_major_formatter(mdates.DateFormatter("%m/%d %H"))
+    ax3.xaxis.set_major_formatter(mdates.DateFormatter("%d %H"))
+    ax3.set_ylim([-600, 600])
+    ax3.tick_params(axis="x", rotation=rot)
+    ax3.set_ylabel("Forcing (W/m$^{2}$)")
+    ax3.legend(loc=[0.01, 0.62])
+
+    if savefigs:
+        plt.savefig(out_dir + "lwd_swd_forcing_short_timespan.png")
+
+
 pyr_dir = swd_params.STAND_DIR
 pyr_fmt = swd_params.PYR_FILEFORMAT
 
@@ -35,8 +207,8 @@ fig2name = out_dir + "lwd_swd_zoom.png"
 utc = dt.timezone.utc
 
 # Run flags
-save_libradtran_tofile = False
-savefigs = True
+get_libradtran_and_savetofile = False  # Slow step
+savefigs = False
 
 # A longer date range
 # date1 = dt.datetime(2022, 2, 1, tzinfo=utc)
@@ -55,7 +227,7 @@ swd_date_long, swd_long = load_rad_flux(pyr_dir, pyr_fmt, date1, date2)
 
 # If needed, calculate the libradtran results and save to file
 outfile = out_dir + "libradtran.csv"
-if save_libradtran_tofile:
+if get_libradtran_and_savetofile:
     libdate, clear_lib = get_libradtran(swd_date_long, outfile)
 
 # Load the libradtran results
@@ -128,49 +300,6 @@ is2 = i2
 lwf_tot_sw_date = np.interp(swd_tstamp[is1:is2], lwd_tstamp[il1:il2], lwf_tot)
 lwd_force_sw_date = np.interp(swd_tstamp, lwd_tstamp, lwd_force)
 
-rot = 0
-datefmt = "%d"
-plt.figure(num=1)
-ax1 = plt.subplot(211)
-ax3 = plt.subplot(212, sharex=ax1)
-wid = 0.85
-hit = 0.4
-ax1.set_position([0.12, 0.57, wid, hit])
-ax3.set_position([0.12, 0.10, wid, hit])
-
-ax1.plot(swd_date_long[:iswl], swd_long[:iswl], color="red", label="Measured")
-ax1.plot(swd_date_long[iswl:], swd_long[iswl:], color="red")
-ax1.plot(swd_date_long[:iswl], swd_clear_lib_long[:iswl], "k--", label="Clear")
-ax1.plot(swd_date_long[iswl:], swd_clear_lib_long[iswl:], "k--")
-# global down
-# ax1.plot(swd_date[ind], swd_clear_lib[:, 0], "ks")  # direct
-# ax1.plot(swd_date[ind], swd_clear_lib[:, 2], "c.")  # diffuse down
-# ax1.plot(swd_date[ind], swd_clear_lib[:, 3], "m+")  # diffuse up
-ax1.legend()
-ax1.set_xlim(
-    dt.datetime(2022, 2, 5, tzinfo=utc), dt.datetime(2022, 2, 11, tzinfo=utc)
-)
-ax1.set_ylim([0, 1000])
-ax1.xaxis.set_major_formatter(mdates.DateFormatter(datefmt))
-ax1.tick_params(axis="x", rotation=rot)
-ax1.set_ylabel("SWD (W/m$^{2}$)")
-# ax1.legend(loc=(0.1,.3))
-
-ax3.plot(lwd_date[:ilw], lwd[:ilw], color="blue", label="Measured")
-ax3.plot(lwd_date[ilw:], lwd[ilw:], color="blue")
-ax3.plot(lwd_clear_date, lwd_clear, "o:", color="blue", label="Clear")
-ax3.legend()
-ax3.xaxis.set_major_formatter(mdates.DateFormatter(datefmt))
-ax3.tick_params(axis="x", rotation=rot)
-ax3.set_xlabel("Day of 2022/02")
-ax3.set_ylabel("LWD (W/m$^{2}$)")
-ax3.set_ylim([0, 1000])
-ax3.legend(loc="upper left")
-
-if savefigs:
-    plt.savefig(fig1name)
-
-
 # ax1 = plt.subplot(221)
 # ax2 = plt.subplot(222, sharex=ax1)
 # ax3 = plt.subplot(223, sharex=ax1)
@@ -199,71 +328,9 @@ if savefigs:
 totf = swf_tot + lwf_tot_sw_date
 totfstr = str(round(totf[-1] / 1e6))
 
-rot = 40
-datefmt = "%d %H"
-fdate = swd_date[isw - 1]
-# fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-plt.close(2)
-plt.figure(num=2)
-ax1 = plt.subplot(221)
-ax2 = plt.subplot(222, sharex=ax1)
-ax3 = plt.subplot(223, sharex=ax1)
-ax4 = plt.subplot(224, sharex=ax1)
-wid = 0.37
-hit = 0.36
-ax1.set_position([0.1, 0.62, wid, hit])
-ax2.set_position([0.6, 0.62, wid, hit])
-ax3.set_position([0.1, 0.15, wid, hit])
-ax4.set_position([0.6, 0.15, wid, hit])
-
-ax1.plot(swd_date[:isw], swd[:isw], color="red", label="Meas")
-# ax1.plot(swd_date[:isw], swd_pysolar[:isw], "--", color="red", label="Pysolar")
-ax1.plot(swd_date[:isw], swd_clear_lib_interp[:isw], "k--", label="Clear")
-ax1.plot(swd_date[isw:], swd_clear_lib_interp[isw:], "k--")
-ax1.legend()
-ax1.xaxis.set_major_formatter(mdates.DateFormatter("%d %H"))
-ax1.tick_params(axis="x", rotation=rot)
-ax1.set_xlim(start_date, final_date)
-ax1.set_ylabel("SWD (W/m$^{2}$)")
-ax1.legend(loc=[0.4, 0.7])
-
-ax3.plot(lwd_date[:ilw], lwd[:ilw], color="blue", label="Measured")
-ax3.plot(lwd_clear_date, lwd_clear, "o:", color="blue", label="Clear")
-ax3.legend()
-ax3.xaxis.set_major_formatter(mdates.DateFormatter("%d %H"))
-ax3.tick_params(axis="x", rotation=rot)
-ax3.set_xlabel("Day and Hour in 2022")
-ax3.set_ylabel("LWD (W/m$^{2}$)")
-
-tot_force = swd_force[:isw] + lwd_force_sw_date[:isw]
-ax2.plot(lwd_date[:ilw], lwd_force[:ilw], color="blue", label="LWD")
-ax2.plot(swd_date[:isw], swd_force[:isw], color="red", label="SWD")
-ax2.plot(swd_date[:isw], tot_force, "k", label="Total")
-ax2.plot(lwd_date[:ilw], np.zeros(ilw), "k:")
-ax2.legend()
-ax2.xaxis.set_major_formatter(mdates.DateFormatter("%m/%d %H"))
-ax2.xaxis.set_major_formatter(mdates.DateFormatter("%d %H"))
-ax2.tick_params(axis="x", rotation=rot)
-ax2.set_ylabel("Forcing (W/m$^{2}$)")
-ax2.legend(loc=[0.4, 0.02])
-
-ax4.plot(lwd_date[il1:il2], lwf_tot / 1e6, color="blue", label="LWD")
-ax4.plot(swd_date[is1:is2], swf_tot / 1e6, color="red", label="SWD")
-ax4.plot(swd_date[is1:is2], totf / 1e6, "k", label="Total")
-ax4.plot(swd_date[is2], totf[-1] / 1e6, "ko")
-ax4.plot(lwd_date[il1:il2], np.zeros(il2 - il1), "k:")
-ax4.legend()
-ax4.set_ylabel("Cumulative Forcing (MJ/m$^{2}$)")
-ax4.xaxis.set_major_formatter(mdates.DateFormatter("%d %H"))
-ax4.tick_params(axis="x", rotation=rot)
-ax4.set_xlabel("Day and Hour in 2022")
-ax4.text(
-    dt.datetime(2022, 2, 7, 15, 30, tzinfo=utc), -15, totfstr, fontsize=14
-)
-
-if savefigs:
-    plt.savefig(fig2name)
-
+plot_measured_and_clear()
+plot_meas_clear_forcings_cumulative_shorttime()
+plot_meas_clear_forcings()
 
 # if plotfigs:
 #     # .. Plot it
