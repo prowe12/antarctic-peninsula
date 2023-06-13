3aa9a13d751e6c78c57b2f9f7449397f94c3a003
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-06-13 08:30:33 -0700

    Add Marambio params

---
+++ antarc/marambio/parameters/mbio_params.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 28 11:21:08 2022
+
+@author: prowe
+"""
+
+# Location
+# Wikipedia Coordinates: 64°14′28″S 56°37′36″
+LOCATION = "Marambio"
+LATITUDE = -64.233  # negative for south
+LONGITUDE = -56.717  # negative for west
