448f06a808d0bdefaf1f997174020af5a32c229a
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-10-03 15:49:44 -0700

    parameter updates

--- antarc/escudero/parameters/atm_prof_params.py
+++ antarc/escudero/parameters/atm_prof_params.py
@@ -4,6 +4,19 @@
 Created on Mon Aug  1 14:20:11 2022
 
 @author: prowe
+
+Sondes:
+2017/01/12 - 2017/01/31: Done
+2017/11/28 - 2017/12/13: Done
+2018/01/04 - 2018/02/20: Done
+2018/11/30 - 2018/12/31: Done
+2019/01/01 - 2019/02/22: Done
+2019/09/02 - 2019/10/03: Done
+2020: No soundings
+2021: No soundings
+2022/01/21 - 2022/08/28: Done
+2023/03/20 - 2023/08/10: Carbon Tracker not available yet (2023/08/23)
+
 """
 # # # # # # # # #    INPUTS   # # # # # # # # # #
 # Parameter modules - specific to user
@@ -13,28 +26,49 @@ MEAS_DIR = params.MEAS_DIR
 PROJ_DIR = params.PROJECT_DIR
 
 # Directories
-main_dir = PROJ_DIR + "NSF_AERI_cloud_CRI/McMurdo/case_studies/"
-yopp_dir = PROJ_DIR + "YOPP/"
-CO2_DIR = MEAS_DIR + "Escudero/co2/"
 SONDE_DIR = MEAS_DIR + "Escudero/radiosondes/datadenial/"
 ERA_DIR = MEAS_DIR + "Escudero/era5/"
 OUT_DIR = MEAS_DIR + "Escudero/profiles/"
 FIG_DIR = MEAS_DIR + "Escudero/profiles//figures/"
-surfmet_dir = main_dir + "met/"
+FREI_MET_DIR = MEAS_DIR + "Escudero/frei_met/by_month_every_15min/"
+TARP_MET_DIR = MEAS_DIR + "Escudero/aws/json_daily_backup/"
+CO2_DIR = MEAS_DIR + "Escudero/co2/"
+CT_DIR = {
+    2017: MEAS_DIR + "Escudero/co2/ct_2022/",
+    2018: MEAS_DIR + "Escudero/co2/ct_2022/",
+    2019: MEAS_DIR + "Escudero/co2/ct_2022/",
+    2022: MEAS_DIR + "Escudero/co2/ct_nrt_2023_4/",
+    2023: MEAS_DIR + "Escudero/co2/ct_nrt_2023_4/",
+}
 
 # Filenames and formats
-SONDE_FILEFORMAT = "esc_sonde_dd_v1_%Y%m%d%H.txt"
-# CT_FILEFORMAT = "CT-NRT.v2022-1.molefrac_glb3x2_%Y-%m-%d.nc"
-CT_FILEFORMAT = "CT-NRT.v2023-2.molefrac_glb3x2_%Y-%m-%d.nc"
+SONDE_FILEFORMAT = "esc_sonde_dd_v2_%Y%m%d%H.txt"
+CT_FILEFORMAT = {
+    2017: "CT2022.molefrac_glb3x2_%Y-%m-%d.nc",
+    2018: "CT2022.molefrac_glb3x2_%Y-%m-%d.nc",
+    2019: "CT2022.molefrac_glb3x2_%Y-%m-%d.nc",
+    2022: "CT-NRT.v2023-4.molefrac_glb3x2_%Y-%m-%d.nc",
+    2023: "CT-NRT.v2023-4.molefrac_glb3x2_%Y-%m-%d.nc",
+}
 ERA_FILEFORMAT = "era5_esc_%Y%m%d.nc"
-
 CO2_FILE_PALMER = CO2_DIR + "co2_psa_surface-flask_1_ccgg_event.txt"
 CO2_FILE_DRAKE = CO2_DIR + "co2_drp_shipboard-flask_1_ccgg_event.txt"
-# surfmet_file = 'awrmetM1.b1.20160208.000000.cdf'
+# FREI_MET_FILE = "esc_met_201901.txt"
+# TARP_MET_FILE = "950007_20210215.json"
+
+TARP_MET_FILEFORMAT = "950007_%Y%m%d.json"
+
+# FREI
+FREI_MET_FILEFORMAT = "esc_met_%Y%m.txt"
+FREI_MET_STATION = 950001
+FREI_MET_LAT = -62.19194
+FREI_MET_LON = -58.97972
+FREI_MET_ALT = 45.0 / 1000  # km
 
 MODEL_EXTRA = 3  # model number for unspecified moledule concentrations
 
 # Layer boundaries (refine as needed for desired temperature differential)
+# fmt: off
 LAYERBNDS = [
     0.0,
     0.025,
@@ -106,3 +140,4 @@ LAYERBNDS = [
     48.5,
     60.0,
 ]
+# fmt: on
---
+++ antarc/escudero/parameters/esc201701.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 28 11:21:08 2022
+
+@author: prowe
+"""
+
+import datetime as dt
+import pytz
+
+DTIME = dt.datetime(2017, 1, 1, 0, 0, 0, tzinfo=pytz.utc)
+DATE1 = dt.datetime(2017, 1, 1, 0, 0, 0, tzinfo=pytz.utc)
+DATE2 = dt.datetime(2017, 1, 31, 0, 0, 0, tzinfo=pytz.utc)
