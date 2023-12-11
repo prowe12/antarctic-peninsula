03400f5d0c6a54486386729b74e5837bc9441dd7
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-12-11 09:41:31 -0800

    Make sure input directory includes the folder for the starting year; get year for any later years in the requested date range and append to all filenames so that time spans longer than one year can be processed at once; add function load_rad_flux_utc that loads downard radiative flux between two dates for timezone unaware dates

--- antarc/load_rad_flux.py
+++ antarc/load_rad_flux.py
@@ -13,7 +13,7 @@ import pytz
 import pandas as pd
 
 # My modules
-from antarc.getfilenames import getfiles_for_daterange
+from antarc.getfilenames import getfiles_for_daterange, getfnames_daterange_utc
 
 
 def load_rad_flux(
@@ -26,13 +26,27 @@ def load_rad_flux(
     @param start_dt  Starting date
     @param end_dt  Ending date
     """
-    files, _ = getfiles_for_daterange(direc, fmt, start_dt, end_dt)
+
+    if direc[-5:] != str(start_dt.year) + "/":
+        raise ValueError("Directory format is incorrect")
+
+    files = []
+    files_wo_direc = []
+    for year in range(start_dt.year, end_dt.year + 1):
+        direc = direc[:-5] + str(year) + "/"
+        files0, _ = getfiles_for_daterange(direc, fmt, start_dt, end_dt)
+        files += [direc + x for x in files0]
+        files_wo_direc += files0
+
+    # QC that the same file does not exist in different year directories
+    if len(files_wo_direc) != len(set(files_wo_direc)):
+        raise ValueError("Same file found in two different directories")
 
     # Load the pyranometer data
     dtimes = np.array([])
     flux = np.array([])
     for i, file in enumerate(files):
-        pir = pd.read_csv(direc + file)
+        pir = pd.read_csv(file)
 
         # Get dates from pyranometer file data
         ntimes = len(pir)
@@ -41,6 +55,55 @@ def load_rad_flux(
             dt.datetime.strptime(pir["Date"][i] + pir["Time"][i], fmt).replace(
                 tzinfo=pytz.utc
             )
+            for i in range(ntimes)
+        ]
+        dtimes = np.hstack([dtimes, dtimes0])
+        flux = np.hstack([flux, pir["Radiation"].values])
+
+    inds = np.where(np.logical_and(dtimes >= start_dt, dtimes <= end_dt))[0]
+
+    return dtimes[inds], flux[inds]
+
+
+def load_rad_flux_utc(
+    direc: str, fmt: str, start_dt: dt.datetime, end_dt: dt.datetime
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Load all downward radiative flux data between two dates
+    assuming all dates are already in UTC, and thus we can use
+    timezone unaware dates
+    @param direc  Directory
+    @param fmt  File format
+    @param start_dt  Starting date (in UTC; timezone unaware)
+    @param end_dt  Ending date (in UTC; timezone unaware)
+    """
+
+    if direc[-5:] != str(start_dt.year) + "/":
+        raise ValueError("Directory format is incorrect")
+
+    files = []
+    files_wo_direc = []
+    for year in range(start_dt.year, end_dt.year + 1):
+        direc = direc[:-5] + str(year) + "/"
+        files0, _ = getfnames_daterange_utc(direc, fmt, start_dt, end_dt)
+        files += [direc + x for x in files0]
+        files_wo_direc += files0
+
+    # QC that the same file does not exist in different year directories
+    if len(files_wo_direc) != len(set(files_wo_direc)):
+        raise ValueError("Same file found in two different directories")
+
+    # Load the pyranometer data
+    dtimes = np.array([])
+    flux = np.array([])
+    for i, file in enumerate(files):
+        pir = pd.read_csv(file)
+
+        # Get dates from pyranometer file data
+        ntimes = len(pir)
+        fmt = "%Y-%m-%d%H:%M:%S"
+        dtimes0 = [
+            dt.datetime.strptime(pir["Date"][i] + pir["Time"][i], fmt)
             for i in range(ntimes)
         ]
         dtimes = np.hstack([dtimes, dtimes0])
