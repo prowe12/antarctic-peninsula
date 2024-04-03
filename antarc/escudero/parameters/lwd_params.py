b0773166ef02a9cb22dfb24258acea6e797fda9f
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2024-04-03 10:48:08 -0700

    Add parameter for 0-238 wavenumbers

--- antarc/escudero/parameters/lwd_params.py
+++ antarc/escudero/parameters/lwd_params.py
@@ -12,5 +12,6 @@ LWD_CLEAR_DIR = "/Users/prowe/Sync/measurements/Escudero/lwd_clear/"
 
 # Files and formats
 LWD_FILEFORMAT = "esc_lwd%Y%m%d_%H%M.csv"
+LWD_CLEAR_FILEFORMAT_OUTNU = "flux%Y%m%d_%H%M_0_238.txt"
 LWD_CLEAR_FILEFORMAT_LOWNU = "flux%Y%m%d_%H%M_238_1500.txt"
 LWD_CLEAR_FILEFORMAT_HINU = "flux%Y%m%d_%H%M_1500_2222.txt"
