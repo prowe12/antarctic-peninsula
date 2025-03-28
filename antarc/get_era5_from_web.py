69bbf1eb6bcb296aa9042643e94284b2f82a4ea0
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2025-03-27 18:32:40 -0700

    Increase options for downloads of ERA5 data

--- antarc/get_era5_from_web.py
+++ antarc/get_era5_from_web.py
@@ -6,6 +6,7 @@ Created on Mon Aug  1 13:36:00 2022
 @author: prowe
 """
 
+from os.path import exists
 import cdsapi
 
 
@@ -42,6 +43,10 @@ def get_params(datestr: list[str], times, latlon, press_levs):
             latlon["east"],
         ],
     }
+    # Uncomment to add cloud info
+    # "specific_cloud_liquid_water_content",
+    # "specific_cloud_ice_water_content",
+    # "fraction_of_cloud_cover",
 
 
 def get_outfile(prefix: str, datestring: str, ext: str = ".nc"):
@@ -128,8 +133,130 @@ def get_era5(outdir, paramsets, dataset):
     c_era = cdsapi.Client()
     for paramset in paramsets:
 
+        outdir0 = f"{outdir}{paramset['params']['year']}/"
+
+        if exists(outdir0 + paramset["outfile"]):
+            continue
         # retrieve the path to the file
         fid = c_era.retrieve(dataset, paramset["params"])
 
         # download the file and save it to the output directory
-        fid.download(outdir + paramset["outfile"])
+        fid.download(outdir0 + paramset["outfile"])
+
+
+def get_paramsets_monthly(
+    prefix,
+    yearstr,
+    monthstr,
+    latlon,
+    press_levs,
+):
+    """
+    Get dictionaries with the output file names and the parameters
+    dictionary of parameters needed for retrieving ERA5 data
+    and pack it together into
+    @returns  A list of the parameter structures
+    @returns  The output file names
+    """
+
+    paramsets = []
+
+    paramset = get_paramset_monthly(
+        prefix,
+        yearstr,
+        monthstr,
+        latlon,
+        press_levs,
+    )
+
+    paramsets.append(paramset)
+    return paramsets
+
+
+def get_paramset_monthly(
+    prefix,
+    yearstr,
+    monthstr,
+    latlon,
+    press_levs,
+):
+    """
+    Get dictionaries with the output file names and the parameters
+    dictionary of parameters needed for retrieving ERA5 data
+    and pack it together into
+    @returns  A list of the parameter structures
+    @returns  The output file names
+    """
+
+    outfile = prefix + ".nc"
+
+    # api parameters:
+    # area: [north, west, south, east]; west is negative
+
+    params = {
+        "format": "netcdf",
+        "product_type": "monthly_averaged_reanalysis",
+        "variable": [
+            "temperature",
+            "relative_humidity",
+        ],
+        "pressure_level": press_levs,
+        "year": yearstr,
+        "month": monthstr,
+        "time": "00:00",
+        "grid": [0.25, 0.25],
+        "area": [
+            latlon["north"],
+            latlon["west"],
+            latlon["south"],
+            latlon["east"],
+        ],
+    }
+    # Uncomment to add cloud info
+    # "specific_cloud_liquid_water_content",
+    # "specific_cloud_ice_water_content",
+    # "fraction_of_cloud_cover",
+
+    # "variable": [
+    #     "pressure",
+    #     "Temperature",
+    #     "Relative humidity",
+    #     "Ozone mass mixing ratio",
+    #     "u_component_of_wind",
+    #     "v_component_of_wind",
+    # ],
+
+    return {"outfile": outfile, "params": params}
+
+
+# c.retrieve(
+#     'reanalysis-era5-pressure-levels-monthly-means',
+#     {
+#         'format': 'netcdf',
+#         'product_type': 'monthly_averaged_reanalysis',
+#         'variable': [
+#             'fraction_of_cloud_cover', 'relative_humidity', 'specific_cloud_ice_water_content',
+#             'specific_cloud_liquid_water_content', 'temperature', 'u_component_of_wind',
+#             'v_component_of_wind',
+#         ],
+#         'pressure_level': [
+#             '30', '50', '70',
+#             '100', '125', '150',
+#             '175', '200', '225',
+#             '250', '300', '350',
+#             '400', '450', '500',
+#             '550', '600', '650',
+#             '700', '750', '775',
+#             '800', '825', '850',
+#             '875', '900', '925',
+#             '950', '975', '1000',
+#         ],
+#         'year': '2018',
+#         'month': '01',
+#         'time': '00:00',
+#         'area': [
+#             90, -180, -90,
+#             180,
+#         ],
+#     },
+#     'download.nc')
