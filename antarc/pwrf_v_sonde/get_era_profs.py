9e9b7e5753656f40adb4a5a08ea3bab915cfec2e
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2025-03-31 22:12:53 -0700

    Minor changes to generalize

--- antarc/pwrf_v_sonde/get_era_profs.py
+++ antarc/pwrf_v_sonde/get_era_profs.py
@@ -10,7 +10,7 @@ import datetime as dt
 
 
 # Modules
-from antarc.get_atm_profs_rsrc import FileInfo, Era
+from antarc.pwrf_v_sonde.get_atm_profs_rsrc import FileInfo, Era
 from antarc.get_era5_press_levs_from_web import get_era5_press_levs
 
 
@@ -18,15 +18,17 @@ from antarc.get_era5_press_levs_from_web import get_era5_press_levs
 def get_era(prefix, params, atm_prof_params, date_str, date_fmt):
     lat = params.LATITUDE
     lon = params.LONGITUDE
-    direc = atm_prof_params.ERA_DIR
+    dtime = dt.datetime.strptime(date_str, date_fmt)
+
+    direc = f"{atm_prof_params.ERA_DIR}{dtime.year}/"
     era_fileformat = atm_prof_params.ERA_FILEFORMAT
 
     # Get the ERA5 files form the web if needed
-    dtime = dt.datetime.strptime(date_str, date_fmt)
     date1 = dt.datetime(dtime.year, dtime.month, dtime.day)
     get_era5_press_levs(lat, lon, date1, date1, direc, prefix, False)
 
     Files = FileInfo(direc, era_fileformat)
 
     # Get the ERA5 data
-    return Era(Files, era_fileformat, dtime, lat, lon)
+    era = Era(Files, era_fileformat, dtime, lat, lon)
+    return era
